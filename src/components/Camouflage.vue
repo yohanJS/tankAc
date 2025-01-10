edee3ee7519dad2a27708b66f86d54984b4e4ae2
Yohan Garcia <yoanvaldes03@gmail.com>
2025-01-09 22:50:08 -0500

    - making the camo blacker

--- src/components/Camouflage.vue
+++ src/components/Camouflage.vue
@@ -1,12 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-      <div class="container p-0">
-    <img src="../assets/rect.png" class="img-fluid w-100" style="height: 50px;">
-  </div>
-</template>
-
-<script>
-</script>
-
-<style scoped>
-</style>
+    <div class="container p-0 position-relative">
+      <img src="../assets/rect.png" class="img-fluid w-100" style="height: 50px;">
+      <div class="overlay"></div>
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped>
+  .container {
+    position: relative;
+  }
+  .container .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5); /* Adjust opacity as needed */
+    z-index: 1;
+  }
+  </style>
+  
